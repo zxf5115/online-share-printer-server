ddd54ab0e78de42297152fb83e59220bfae703e7
Zhang <1326336909@qq.com>
2022-01-14 16:26:41 +0800

    新增主流银行模型类

---
+++ app/Models/Platform/Module/Common/Bank.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models\Platform\Module\Common;
+
+use App\Models\Common\Module\Common\Bank as Common;
+
+
+/**
+ * @author zhangxiaofei [<1326336909@qq.com>]
+ * @dateTime 2022-01-14
+ *
+ * 主流银行模型类
+ */
+class Bank extends Common
+{
+
+}
