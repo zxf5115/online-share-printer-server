c5c321e76dc50b1e5689d5447d185b28df31161f
Zhang <1326336909@qq.com>
2022-03-09 20:14:20 +0800

    修复问题: 按区域统计销售额和按级别统计销售额问题

--- app/Http/Controllers/Platform/System/IndexController.php
+++ app/Http/Controllers/Platform/System/IndexController.php
@@ -11,6 +11,7 @@ use App\Models\Platform\Module\Manager;
 use App\Models\Platform\Module\Printer;
 use App\Models\Common\Module\Common\Area;
 use App\Http\Controllers\Platform\BaseController;
+use App\Models\Platform\Module\Organization\Archive;
 use App\Models\Platform\Module\Education\Courseware;
 
 /**
@@ -182,44 +183,88 @@ class IndexController extends BaseController
    * @param Request $request [description]
    * @return [type]
    */
-  public function agentsales(Request $request)
+  public function area(Request $request)
   {
     try
     {
-      $line = [];
-      $order_total = 0;
+      $response = [];
 
-      // 统计时间区间
-      $condition = self::getWhereCondition($request->type);
+      $condition = self::getSimpleWhereData($request->area_id, 'province_id');
 
-      // 订单总数
-      $order_total = Order::getCount($condition);
+      // 获取对应区域的代理商编号
+      $member_id =Archive::getPluck('member_id', $condition, false, false, true);
+
+      if(empty($member_id))
+      {
+        return self::success($response);
+      }
+
+      $condition = self::getSimpleWhereData();
+
+      $where = [
+        ['id', $member_id]
+      ];
+
+      $condition = array_merge($condition, $where);
+
+      $organization_id = Agent::getPluck('id', $condition, false, false, true);
+
+      if(empty($organization_id))
+      {
+        return self::success($response);
+      }
+
+      $condition = self::getSimpleWhereData();
+
+      $where = [
+        ['first_level_agent_id', $organization_id]
+      ];
+
+      $condition = array_merge($condition, $where);
 
       $order = Order::getList($condition);
 
-      $orderDate = [];
-
-      foreach($order as $item)
+      if(empty($order))
       {
-        $orderDate[] = date('Y-m-d', strtotime($item->create_time));
+        return self::success($response);
       }
 
-      $orderDate = array_count_values($orderDate);
-
-      foreach($orderDate as $key => $item)
+      foreach($order as $key => $item)
       {
-        $orderData[] = [
-          'title' => $key,
-          '销售额' => $item,
+        if(1 == $item->pay_status['value'])
+        {
+          $result[$item->first_level_agent_id][] = $item->pay_money;
+        }
+      }
+
+      if(empty($result))
+      {
+        return self::success($response);
+      }
+
+      foreach($result as $key => $item)
+      {
+        $money = 0;
+
+        foreach($item as $vo)
+        {
+          $money = bcadd($money, $vo, 2);
+        }
+
+        $response[] = [
+          'title' => Agent::getValue('nickname', ['id' => $key]),
+          '销售额' => $money,
         ];
       }
 
-      $sort = array_column($orderData, 'title');
+      // 提取销售额
+      $value = array_column($response, '销售额');
 
-      array_multisort($orderData, SORT_ASC, $sort);
+      // 按照销量额进行从大到小排序
+      array_multisort($value, SORT_DESC, $response);
 
-      $response['order_total'] = $order_total;
-      $response['line'] = $orderData;
+      // 只返回前10位
+      $response = array_slice($response, 0, 10);
 
       return self::success($response);
     }
@@ -233,57 +278,93 @@ class IndexController extends BaseController
   }
 
 
-
-
   /**
    * @author zhangxiaofei [<1326336909@qq.com>]
-   * @dateTime 2021-06-30
+   * @dateTime 2021-10-29
    * ------------------------------------------
-   * 订单统计数据
+   * 获取各级代理商市场销售额
    * ------------------------------------------
    *
-   * 订单统计数据
+   * 获取各级代理商市场销售额
    *
+   * @param Request $request [description]
    * @return [type]
    */
-  public function data(Request $request)
+  public function level(Request $request)
   {
     try
     {
-      $line = [];
-      $order_total = 0;
+      $response = [];
 
-      // 统计时间区间
-      $condition = self::getWhereCondition($request->type);
+      $condition = self::getSimpleWhereData($request->level, 'level');
 
-      // 订单总数
-      $order_total = Order::getCount($condition);
+      $organization_id = Agent::getPluck('id', $condition, false, false, true);
+
+      if(empty($organization_id))
+      {
+        return self::success($response);
+      }
+
+      $condition = self::getSimpleWhereData();
+
+      if(1 == $request->level)
+      {
+        $field = 'first_level_agent_id';
+      }
+      else
+      {
+        $field = 'second_level_agent_id';
+      }
+
+      $where = [
+        [$field, $organization_id]
+      ];
+
+      $condition = array_merge($condition, $where);
 
       $order = Order::getList($condition);
 
-      $orderDate = [];
-
-      foreach($order as $item)
+      if(empty($order))
       {
-        $orderDate[] = date('Y-m-d', strtotime($item->create_time));
+        return self::success($response);
       }
 
-      $orderDate = array_count_values($orderDate);
-
-      foreach($orderDate as $key => $item)
+      foreach($order as $key => $item)
       {
-        $orderData[] = [
-          'title' => $key,
-          '订单数' => $item,
+        if(1 == $item->pay_status['value'])
+        {
+          $result[$item->$field][] = $item->pay_money;
+        }
+      }
+
+      if(empty($result))
+      {
+        return self::success($response);
+      }
+
+      foreach($result as $key => $item)
+      {
+        $money = 0;
+
+        foreach($item as $vo)
+        {
+          $money = bcadd($money, $vo, 2);
+        }
+
+        $response[] = [
+          'title' => Agent::getValue('nickname', ['id' => $key]),
+          '销售额' => $money,
         ];
       }
 
-      $sort = array_column($orderData, 'title');
+      // 提取销售额
+      $value = array_column($response, '销售额');
 
-      array_multisort($orderData, SORT_ASC, $sort);
+      // 按照销量额进行从大到小排序
+      array_multisort($value, SORT_DESC, $response);
 
-      $response['order_total'] = $order_total;
-      $response['line'] = $orderData;
+      // 只返回前10位
+      $response = array_slice($response, 0, 10);
 
       return self::success($response);
     }
